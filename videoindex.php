5468f6cf370dfd65f7e8d573de9da21815794af2
lg03214456 <64246868+lg03214456@users.noreply.github.com>
2021-05-11 17:33:51 +0800

    Add files via upload

---
+++ videoindex.php
@@ -0,0 +1,29 @@
+<?php include("header.php"); ?>
+<div class = "container">
+<?php include("menu.php"); ?>
+<?php
+include("database.php");
+$sql = "SELECT * FROM video";
+$result = $conn->query($sql);
+
+?>
+<?php
+if ($result->num_rows > 0) {
+  echo "<table class = 'table table-striped'>";
+  echo "<tr><td>影片名稱</td><td>張貼時間</td></tr>";
+  while($row = $result->fetch_assoc()) {
+  	echo "<tr>";
+    	echo "<td><a href='play.php?id=" . $row["id"]. "'>" . $row["title"]. "</a><br>" . "<img src ='https://i.ytimg.com/vi/". $row["vid"] ."/hqdefault.jpg'width=150></td> "; 
+    	echo "<td>" . $row["created"]. "</td> "; 
+    echo "</tr>";	
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+</div>
+<?php include("footer.php"); ?>
+ 
